a2f8fa179c3e78fcdf76fc206adc2d117abc8bf3
haveneg <havenedward24@gmail.com>
2023-03-09 01:12:16 -0500

    initial commit with git in command line

---
+++ Player2.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+func _ready(): 
+	screen_size = get_viewport_rect().size
+
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("p2_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("p2_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("p2_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("p2_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
