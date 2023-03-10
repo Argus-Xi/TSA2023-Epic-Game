af7e852a30eace157fb37e91f56860c6e0cf8e3c
haveneg <97711931+haveneg@users.noreply.github.com>
2023-03-09 22:34:05 -0500

    new sprites for tanks

--- Player1.gd
+++ Player1.gd
@@ -2,26 +2,35 @@ extends Node2D
 
 export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
+export (float) var rotation_speed = 1.5
 
 func _ready(): 
 	screen_size = get_viewport_rect().size
 
-func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
+var velocity = Vector2()
+var rotation_dir = 0
+
+func get_input():
+	rotation_dir = 0
+	velocity = Vector2()
 	if Input.is_action_pressed("p1_right"):
-		velocity.x += 1
+		rotation_dir += 1
 	if Input.is_action_pressed("p1_left"):
-		velocity.x -= 1
+		rotation_dir -= 1
 	if Input.is_action_pressed("p1_down"):
-		velocity.y += 1
+		velocity = Vector2(-speed, 0).rotated(rotation)
 	if Input.is_action_pressed("p1_up"):
-		velocity.y -= 1
+		velocity = Vector2(speed, 0).rotated(rotation)
+
+func _physics_process(delta):
+	get_input()
+	rotation += rotation_dir * rotation_speed * delta
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$AnimatedSprite.play()
-	else:
-		$AnimatedSprite.stop()
+	#	$AnimatedSprite.play()
+	#else:
+	#	$AnimatedSprite.stop()
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
