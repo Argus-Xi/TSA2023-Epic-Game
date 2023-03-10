59c680f425ef55005708f8caa53cf73e410a10a2
haveneg <havenedward24@gmail.com>
2023-03-09 21:45:07 -0500

    replaced tank sprites

--- Player1.gd
+++ Player1.gd
@@ -28,9 +28,9 @@ func _physics_process(delta):
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$AnimatedSprite.play()
-	else:
-		$AnimatedSprite.stop()
+	#	$AnimatedSprite.play()
+	#else:
+	#	$AnimatedSprite.stop()
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
