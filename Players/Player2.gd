37ec011719b86248b9b07520df00d0b84d3a8a50
haveneg <97711931+haveneg@users.noreply.github.com>
2023-03-11 17:26:21 -0500

    Players in folder, Terrain base tile maps and test levels in terrain folder, first level testing in actual stages, collision layers defined

--- Players/Player2.gd
+++ Players/Player2.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends KinematicBody2D
 
 export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
@@ -7,6 +7,7 @@ export (float) var rotation_speed = 1.5
 func _ready(): 
 	screen_size = get_viewport_rect().size
 
+#  Where the body will move
 var velocity = Vector2()
 var rotation_dir = 0
 
@@ -18,29 +19,18 @@ func get_input():
 	if Input.is_action_pressed("p2_left"):
 		rotation_dir -= 1
 	if Input.is_action_pressed("p2_down"):
-		velocity = Vector2(-speed, 0).rotated(rotation)
+		velocity = Vector2(-1, 0).rotated(rotation)
 	if Input.is_action_pressed("p2_up"):
-		velocity = Vector2(speed, 0).rotated(rotation)
+		velocity = Vector2(1, 0).rotated(rotation)
 
 func _physics_process(delta):
 	get_input()
+	# rotates the player
 	rotation += rotation_dir * rotation_speed * delta
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-	#	$AnimatedSprite.play()
-	#else:
-	#	$AnimatedSprite.stop()
-	position += velocity * delta
+	# normalizes the velocity's direction and multiplying it by the speed and the time passed between frames
+	velocity = velocity.normalized() * speed * delta
+
+	move_and_collide(velocity)
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
