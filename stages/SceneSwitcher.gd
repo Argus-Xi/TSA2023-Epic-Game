54c098135cf28349691d11e860919c8ff0b2b5b4
haveneg <97711931+haveneg@users.noreply.github.com>
2023-03-10 21:37:38 -0500

    Initiated Scene Switcher, which will be the main scene, and got a simple looping stage system working

--- stages/SceneSwitcher.gd
+++ stages/SceneSwitcher.gd
@@ -1,6 +1,12 @@
 extends Node
 
-onready var current_level = $Menu
+onready var level_container = $LevelContainer  # child that contains the levels below the UI of SceneSwitcher
+
+onready var current_level = $LevelContainer/TitleScreen  # Initiates current_level variable with the Title Screen
+
+onready var menu_screen = $MenuScreen  # menu screen UI reference
+
+var max_number_levels = 3   # Set number of levels to be cycled through (including Level0 or TitleScreen)
 
 func _ready() -> void:
 	current_level.connect("level_changed", self, "handle_next_level")
@@ -8,9 +14,13 @@ func _ready() -> void:
 func handle_next_level(current_level_number: int):
 	print("handle next level function has been called")
 	var next_level
-	
+	if current_level_number + 1 == max_number_levels:  # checks whether or not it's the last level, cycles back to title screen if it is
+		current_level_number = -1
 	next_level = load("res://stages/Level" + str(current_level_number + 1) + ".tscn").instance()
-	add_child(next_level)
+	level_container.add_child(next_level)
 	next_level.connect("level_changed", self, "handle_next_level")
 	current_level.queue_free()
 	current_level = next_level
+
+func _on_MenuButton_pressed():
+	menu_screen.visible = not menu_screen.visible
--- stages/TitleScreen.gd
+++ stages/TitleScreen.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+
 signal level_changed
 
 export var level_number = 0
 
 
 func _on_NextButton_pressed():
+	print("next button is pressed")
 	emit_signal("level_changed", level_number)
